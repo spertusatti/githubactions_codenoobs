78a1909a164f04322910cbf72df46c3dc2a83b17
spertusatti <spertusatti@plainconcepts.com>
2022-02-19 12:55:28 +0100

    Added readme, update layout

--- src/components/TasksTable.jsx
+++ src/components/TasksTable.jsx
@@ -4,6 +4,7 @@ import Table from "react-bootstrap/Table";
 export const TasksTable = ({ tasks }) => {
   const TableHeader = () => (
     <tr>
+      <th>id</th>
       <th>Assigned to</th>
       <th>Priority</th>
       <th>CreatedBy</th>
@@ -14,6 +15,7 @@ export const TasksTable = ({ tasks }) => {
 
   const TableRow = (task, index) => (
     <tr key={index} className={index % 2 === 0 ? "odd" : "even"}>
+      <td>{task.id}</td>
       <td>{task.AssignedTo}</td>
       <td>{task.Priority}</td>
       <td>{task.CreatedBy}</td>
@@ -29,11 +31,9 @@ export const TasksTable = ({ tasks }) => {
   const tableBody = tasks.map((task, index) => TableRow(task, index));
 
   return (
-    <div className="container">
-      <Table striped responsive="lg">
-        <thead>{TableHeader()}</thead>
-        <tbody>{tableBody}</tbody>
-      </Table>
-    </div>
+    <Table striped responsive="lg">
+      <thead>{TableHeader()}</thead>
+      <tbody>{tableBody}</tbody>
+    </Table>
   );
 };
