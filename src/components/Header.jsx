9f933259c5252911cf6963381c077535bcd122c7
spertusatti <spertusatti@plainconcepts.com>
2022-02-20 11:55:56 +0100

    Lint

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
 export const Header = () => (
-  <div className="header">
+  <div className='header'>
     <h1>Code Noobs Simple Web App ⚡</h1>
   </div>
 );
--- src/components/TasksTable.jsx
+++ src/components/TasksTable.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import Table from "react-bootstrap/Table";
+import React from 'react';
+import Table from 'react-bootstrap/Table';
 
-export const TasksTable = ({ tasks }) => {
+export const TasksTable = ({tasks}) => {
   const TableHeader = () => (
     <tr>
       <th>Assigned to</th>
@@ -13,14 +13,14 @@ export const TasksTable = ({ tasks }) => {
   );
 
   const TableRow = (task, index) => (
-    <tr key={index} className={index % 2 === 0 ? "odd" : "even"}>
+    <tr key={index} className={index % 2 === 0 ? 'odd' : 'even'}>
       <td>{task.AssignedTo}</td>
       <td>{task.Priority}</td>
       <td>{task.CreatedBy}</td>
       <td>{task.CreatedDateTime}</td>
       <td>
-        <div className="row">
-          <div className="col-md-8">{task.State}</div>
+        <div className='row'>
+          <div className='col-md-8'>{task.State}</div>
         </div>
       </td>
     </tr>
@@ -29,8 +29,8 @@ export const TasksTable = ({ tasks }) => {
   const tableBody = tasks.map((task, index) => TableRow(task, index));
 
   return (
-    <div className="container">
-      <Table striped responsive="lg">
+    <div className='container'>
+      <Table striped responsive='lg'>
         <thead>{TableHeader()}</thead>
         <tbody>{tableBody}</tbody>
       </Table>
